1514fa545c57b20c561d4e341b085dff1f78777c
ishan1274 <143828488+ishan1274@users.noreply.github.com>
2023-09-12 01:15:01 +0530

    Add files via upload

---
+++ ArrayBasic1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Extreme prints in array
+
+  // int arr[] = {5,6,7,8,9,1};
+  // int size = 6;
+
+  // int maxi = WINT_MIN;
+  // int mini = WINT_MAX;
+  // for(int i=0; i<size; i++){
+  //   if(arr[i] < mini){
+  //      mini = arr[i];
+  //   }
+  // }
+  // cout << "Maximum no. is " << maxi << endl;
+
+  // int arr[] = {0,1,0,0,0,0,1,1,0,1};
+  // int size = 10;
+
+  // int numZero = 0;
+  // int numOne= 0;
+
+  // for(int i=0; i<size; i++){
+  //   if(arr[i] ==0){
+  //     numZero++;
+  //   }
+  //   if(arr[i] ==0){
+  //     numOne++;
+  //   }
+  // }
+
+  // cout << "The number of zeroes" << numZero << endl;
+  // cout << "The number of ones " << numOne << endl;
+  return 0;
+}
