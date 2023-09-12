2df23390b44adc217e518eb0604d77118b028332
ishan1274 <143828488+ishan1274@users.noreply.github.com>
2023-09-13 01:04:39 +0530

    Add files via upload

---
+++ extreme_print_in_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void extremePrint(int arr[], int size){
+  int left = 0;
+  int right = size-1;
+
+  while(left <= right){
+    if(left == right){
+      cout << arr[left] << endl;
+    }
+    else{
+      cout << arr[left] << endl;
+    cout << arr[right] << endl;
+    }
+    
+    left++;
+    right--;
+  }
+}
+
+int main() {
+  int arr[5] = {1,2,3,4,5};
+  int size = 5;
+
+  extremePrint(arr, size);
+}
